a065b5555b63d1be1a252f81cd582899bd77aefa
Kritsada Chinpala <thor.kritsada@live.com>
2015-02-05 22:17:25 +0700

    เป็นผลงานที่นำผลงานที่ทำระหว่างเรียน
    เป็นผลงานที่นำผลงานที่ทำระหว่างเรียน แล้วนำกลับมาทำใหม่

---
+++ Seminar/Controllers/SeminarController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Seminar.Controllers
+{
+    public class SeminarController : Controller
+    {
+        // GET: Seminar
+        public ActionResult Windows8Camp()
+        {
+            return View();
+        }
+
+        public ActionResult LocationBased()
+        {
+            return View();
+        }
+
+        public ActionResult Dump() 
+        {
+            return View();
+        }
+
+        public ActionResult Asean()
+        {
+            return View();
+        }
+    }
+}
